7516bd8723319ca7ebb86e00548bb56e7a7e927e
ilhanatike <ilhanatike@gmail.com>
2019-07-30 21:32:01 -0400

    added more gits

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,12 @@
 public class AppTest {
 
     public static void main(String[] args) {
-        System.out.println("Hellooogo");
+        System.out.println("Hello");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+
     }
 }
