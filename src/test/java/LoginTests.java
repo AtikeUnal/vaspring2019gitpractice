c96eede0bc06723da87a07e7ce44fd398b9fa10b
ilhanatike <49658446+ilhanatike@users.noreply.github.com>
2019-08-06 21:40:08 -0400

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,10 +3,8 @@ public class LoginTests {
 
     public static void main(String[] args) {
         System.out.println("line 1");
-        System.out.println("line 2");
         
         
-         System.out.println("line 1");
     }
 
 }
