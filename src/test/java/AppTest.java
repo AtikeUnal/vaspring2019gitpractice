355b91dac21805152af6065887b4b1381a961ca2
ilhanatike <49658446+ilhanatike@users.noreply.github.com>
2019-08-06 20:39:47 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,7 +2,7 @@ public class AppTest {
 
     public static void main(String[] args) {
   
-        System.out.println("git");
+        System.out.println("good morning");
         System.out.println("git");
         System.out.println("git");
         System.out.println("gitme");
